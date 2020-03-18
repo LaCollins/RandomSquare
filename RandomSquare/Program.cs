ceb4c8f4f0a56eba4d55555a138c21e81ddb8973
Laura Collins <laura.e.collins1@gmail.com>
2020-03-18 18:32:52 -0500

    done with all steps

--- RandomSquare/Program.cs
+++ RandomSquare/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RandomSquare
 {
@@ -6,7 +7,30 @@ namespace RandomSquare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            List<int> randomNumbers = new List<int>();
+
+            for (int i = 0; i < 20; i += 1)
+            {
+                randomNumbers.Add(random.Next(50));
+            }
+
+            List<int> squaredNumbers = new List<int>();
+
+            foreach (var num in randomNumbers)
+            {
+                squaredNumbers.Add(num * num);
+            }
+
+            foreach (var num in randomNumbers)
+            {
+                var squared = num * num;
+                if (squared % 2 != 0)
+                {
+                    squaredNumbers.Remove(squared);
+                }
+            }
+
         }
     }
 }
